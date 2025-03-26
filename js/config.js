6d48f5013384672b2a017ca417af0b435c4266c2
Nilay Naha <100094390+AmiNilay@users.noreply.github.com>
2025-03-26 23:38:19 +0530

    config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@ const config = {
         { text: "Portfolio", url: "https://aminilay.github.io/nilay-naha-portfolio/" },
         { text: "Tic-Tac-Toe with ai Opponent", url: "https://drive.google.com/file/d/1y5oieLIDpappdMr--xcNXnprVZ9oYCBn/view" },
         { text: "SmartCalculator", url: "https://drive.google.com/file/d/1WMj7qymk7zceD5FVSPgBUgaJ3fFVvZg1/view" },
-        { text: "Personal Link Page (LinkCascade)", url: "#" } // New button
+        { text: "Personal Link Page (LinkCascade)", url: "https://aminilay.github.io/LinkCascade/" } // New button
     ],
     social: {
         github: "https://github.com/AmiNilay",
